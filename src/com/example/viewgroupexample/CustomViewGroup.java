40081fdb67c6f7af79418f90b6fe29f097d1585d
Sergey Lysanov <jewer@ubuntu.(none)>
2013-12-15 02:04:53 +0400

    fling

--- src/com/example/viewgroupexample/CustomViewGroup.java
+++ src/com/example/viewgroupexample/CustomViewGroup.java
@@ -2,7 +2,10 @@ package com.example.viewgroupexample;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -15,33 +18,35 @@ import android.widget.Scroller;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class CustomViewGroup extends ViewGroup {
+	
+	//Gestures
 	private float firstX;
 	private float firstY;
 
-	private int mMaxScrollX;
 	private int mMinScrollX;
 
 	private Scroller mScroller;
 	private GestureDetector mGD;
-
-	public CustomViewGroup(Context context) {
-		super(context);
-		mGD = new GestureDetector(getContext(), mGestureListener);
-		mScroller = new Scroller(getContext());
-		// TODO Auto-generated constructor stub
-	}
+	
+	//colors
+	private int firstColor = Color.BLUE;
+	private int secondColor = Color.RED;
+	private int thirdColor = Color.GREEN;
+	private int fourthColor = Color.MAGENTA;
+	
+	//attributes
+	private int marginRight = 0;
 
 	public CustomViewGroup(Context context, AttributeSet attrs) {
 		super(context, attrs);
 
 		mGD = new GestureDetector(getContext(), mGestureListener);
 		mScroller = new Scroller(getContext());
-	}
-
-	public CustomViewGroup(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		mGD = new GestureDetector(getContext(), mGestureListener);
-		mScroller = new Scroller(getContext());
+		setWillNotDraw(false);
+		
+		TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.SlidingMenu);
+		marginRight = (int) arr.getDimension(R.styleable.SlidingMenu_rightMargin, marginRight);
+		arr.recycle();
 	}
 
 	@Override
@@ -49,12 +54,13 @@ public class CustomViewGroup extends ViewGroup {
 			int bottom) {
 		final View child1 = getChildAt(0);
 		child1.layout(left, top, right, bottom);
-
+		child1.setBackgroundColor(firstColor);
+		
 		final View child2 = getChildAt(1);
 		child2.layout(left - right, top, left, bottom);
+		child2.setBackgroundColor(secondColor);
 
-		mMinScrollX = left - right;
-		mMaxScrollX = right;
+		mMinScrollX = left - right + marginRight;
 	}
 
 	@Override
@@ -98,10 +104,8 @@ public class CustomViewGroup extends ViewGroup {
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		int action = event.getAction();
-
-		mGD.onTouchEvent(event);
-		
+	
+		/*int action = event.getAction();
 		switch (action) {
 		case MotionEvent.ACTION_DOWN:
 			Log.v("TOUCH", "onTouchEvent ACTION_DOWN");
@@ -137,51 +141,98 @@ public class CustomViewGroup extends ViewGroup {
 			break;
 		default:
 			break;
-		}
+		}*/
 
-		return super.onTouchEvent(event);
+		return mGD.onTouchEvent(event);
 	}
-
+	
 	@Override
 	protected void onDraw(Canvas canvas) {
-		// TODO Auto-generated method stub
 		super.onDraw(canvas);
-
-		// scrollTo invalidates, so until animation won't finish it will be
-		// called
-		// (used after a Scroller.fling() )
-		if (mScroller.computeScrollOffset()) {
-			getChildAt(0).setLeft(mScroller.getCurrX());
-			getChildAt(1).setRight(mScroller.getCurrX());
-			// scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
-		}
+		
+		if(mScroller.computeScrollOffset()) {
+	    	//Log.d("GEST", "onDraw X " + Integer.toString(mScroller.getCurrX()));
+	        scrollTo(mScroller.getCurrX(), 0);
+	    }
 	}
 
 	private final GestureDetector.SimpleOnGestureListener mGestureListener = new GestureDetector.SimpleOnGestureListener() {
+		
 		@Override
 		public boolean onDown(MotionEvent e) {
 			if (!mScroller.isFinished()) { // is flinging
 				mScroller.forceFinished(true); // to stop flinging on touch
 			}
+			//Log.d("GEST", "onDown");
 			return true; // else won't work
 		}
 
-		// ...
+		@Override
+		public boolean onSingleTapConfirmed(MotionEvent e) {
+			
+			Log.d("GEST", "onSingleTapConfirmed");
+			int scrollX = Math.abs(getScrollX());
+			int tapX = (int)e.getX();
+			
+			if(tapX > scrollX){
+				///Log.d("GEST", "Tap first view");
+				View firstView = getChildAt(0);
+				ColorDrawable drawable = (ColorDrawable)firstView.getBackground();
+				if(drawable.getColor() == firstColor){
+					firstView.setBackgroundColor(thirdColor);
+				}
+				else{
+					firstView.setBackgroundColor(firstColor);
+				}
+			}
+			else{
+				///Log.d("GEST", "Tap second view");
+				View firstView = getChildAt(1);
+				ColorDrawable drawable = (ColorDrawable)firstView.getBackground();
+				if(drawable.getColor() == secondColor){
+					firstView.setBackgroundColor(fourthColor);
+				}
+				else{
+					firstView.setBackgroundColor(secondColor);
+				}
+			}
+			
+			return true;
+		};
+		
 		@Override
 		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
 				float velocityY) {
-			mScroller.fling(getScrollX(), getScrollY(), -(int) velocityX,
-					-(int) velocityY, mMinScrollX, mMaxScrollX, 0, 0);
-			invalidate(); // don't remember if it's needed
+			
+			//Log.d("GEST", "onFling");
+			//Log.d("GEST", "velocityX" + Float.toString(velocityY));
+
+			mScroller.fling(getScrollX(), getScrollY(),
+	                -(int)velocityX,-(int)velocityY, mMinScrollX, 0, 0, 0);
+			invalidate();
+		    
 			return true;
 		}
 
 		@Override
 		public boolean onScroll(MotionEvent e1, MotionEvent e2,
 				float distanceX, float distanceY) {
+			//Log.d("GEST", "onScroll");
+			//Log.d("GEST", "distanceX " + distanceX);
+			//Log.d("GEST", "getX " + getScrollX());
+			
 			// beware, it can scroll to infinity
-			// scrollBy((int)distanceX, (int)distanceY);
+			if(distanceX + getScrollX() > 0){ //close left menu distanceX < 0
+				distanceX = -getScrollX();
+			}
+			if(distanceX + getScrollX() < mMinScrollX){ //open left menu
+				distanceX = -getScrollX() + mMinScrollX;
+			}
+			
+			scrollBy((int)distanceX, 0);
+			
 			return true;
 		}
 	};
+	
 }
